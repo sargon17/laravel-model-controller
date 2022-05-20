9f4517bd88e0a5248d87397bfd99e1cd23cba542
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-20 18:53:16 +0200

    setted PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class PageController extends Controller
 {
+    public function index()
+    {
+        $movies = Movie::all();
+
+        return view("pages.index", compact("movies"));
+    }
     //
 }
