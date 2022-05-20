c5fda05e7bf97beb70478087a388ffb2c33d7190
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-20 23:09:15 +0200

    ex done

--- app/Movie.php
+++ app/Movie.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     //
+
+    public function changeDateFormat($date)
+    {
+        return date("d-m-Y", strtotime($date));
+    }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,2 +1,14 @@
-<h1>Hello Beautifull Word</h1>
-@dump($movies)
+@extends('../layouts._index')
+
+@section('movies')
+
+@foreach ($movies as $movie)
+    <div class="movie-card">
+        <h2 class="title">{{ $movie->title }}</h2>
+        <p class="original-title">{{ $movie->original_title }}</p>
+        <p class="rating">Rating: {{ $movie->vote }}</p>
+        <p class="release-date">Release: {{ $movie->changeDateFormat($movie->date) }}</p>
+    </div>
+@endforeach
+
+@endsection
